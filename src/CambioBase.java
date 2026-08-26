5a34fd14c084026b56231bf3cf71cc80d5dbdb05
martinn255 <espinosamartin58@gmail.com>
2026-08-26 14:00:38 -0300

    Nuevo

---
+++ src/CambioBase.java
@@ -0,0 +1,18 @@
+public class CambioBase {
+    private int base;
+    private double num;
+    public CambioBase(){
+
+    }
+    //Todos los valores tienen que estar en punto flotante Normalizado y sin normalizar
+    public void sumaPonderada(int base,double num){
+
+    }
+    public void divisionReiterada(int base, double num){
+
+    }
+    public void multiplicacionReiterada(int base,double num){
+
+    }
+
+}
