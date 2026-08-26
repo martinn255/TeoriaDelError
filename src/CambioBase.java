af2150082f0f8e74ec6f5259b3eae9e2a1b07c41
martinn255 <espinosamartin58@gmail.com>
2026-08-26 15:33:01 -0300

    Test Suma

--- src/CambioBase.java
+++ src/CambioBase.java
@@ -1,12 +1,34 @@
+import java.math.*;
 public class CambioBase {
     private int base;
     private double num;
     public CambioBase(){
+       
 
     }
     //Todos los valores tienen que estar en punto flotante Normalizado y sin normalizar
-    public void sumaPonderada(int base,double num){
-
+    public void sumaPonderada(int base, double num){
+        double aux = num;
+        System.out.println(aux);
+        System.out.println(base);
+        int dig=0;
+        int i=0;
+        int suma=0;
+        while (aux!=0 && dig<base){
+            
+            dig=(int)aux%10;
+            System.out.println("Digito:" + dig );
+            aux=(int)aux/10;
+            suma = (int) (dig * Math.pow((double)base, (double)i) + suma);
+            //System.out.println("Suma Final:" + suma);
+            i++;
+        }
+        if(dig>=base){
+            System.out.println("Error En numero No corresponde a la base");
+        }else{
+            System.out.println("Suma Final:" + suma);
+        }
+        
     }
     public void divisionReiterada(int base, double num){
 
@@ -22,9 +44,5 @@ public class CambioBase {
 
 
     /// teste de brancheo
-
-    //asdadasdasd
-    // dadasdasdasdasdasdasdasd 2222222
-    //Hola mundo 
 
 }
