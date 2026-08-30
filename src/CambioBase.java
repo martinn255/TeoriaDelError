1d2bfc36e99d71972e245588e0dd65cf49f4d172
martinn255 <espinosamartin58@gmail.com>
2026-08-30 20:48:56 -0300

    back1:Agrego bases faltantes

--- src/CambioBase.java
+++ src/CambioBase.java
@@ -3,27 +3,31 @@ import java.util.ArrayList;
 
 public class CambioBase {
     private int base;
-    private double num;
+    // private double num;
     public CambioBase(){
        
 
     }
     //Todos los valores tienen que estar en punto flotante Normalizado y sin normalizar
-    public void sumaPonderada(int base, double num){
-        double aux = num;
-        System.out.println(aux);
-        System.out.println(base);
-        int dig=0;
-        int i=0;
-        int suma=0;
-        while (aux!=0 && dig<base){
-            
+    public void sumaPonderada(int base, String num){
+        //double aux = num;
+        int dig=0,i=0;
+        double suma=0;
+        ArrayList<Integer> aux = listaDig(num);
+        /*while (aux!=0 && dig<base){
             dig=(int)aux%10;
-            System.out.println("Digito:" + dig );
             aux=(int)aux/10;
             suma = (int) (dig * Math.pow((double)base, (double)i) + suma);
-            //System.out.println("Suma Final:" + suma);
             i++;
+        }*/
+        i=aux.size()-1;//posicion del vector
+        double exp=0;
+        while(i>=0 && dig<base){
+            dig=aux.get(i);
+            //System.out.println("Digito:" + dig );
+            suma =(dig * Math.pow((double)base, exp) + suma);
+            i--;
+            exp++;
         }
         if(dig>=base){
             System.out.println("Error En numero No corresponde a la base");
@@ -41,23 +45,35 @@ public class CambioBase {
             resto = (int) aux%base;
             System.out.println("resto: " + resto);
             arrayAux.add(resto);
-            aux = (int)aux/2;
+            aux = (int)aux/base;
             System.out.println("aux: " +aux);
         }
 
-        System.out.println(arrayAux.toString());
+        //System.out.println(arrayAux.toString());
 
         int index = arrayAux.size() - 1;
         double convertido = 0;
 
         while(index >= 0) {
             convertido = convertido * 10 + arrayAux.get(index);
-            System.out.println("convertido: " + convertido);
+            //System.out.println("convertido: " + convertido);
             index--;
         }
+        System.out.println("convertido: " + convertido);
     }
     public void multiplicacionReiterada(int base,double num){
 
+    }
+    public ArrayList<Integer> listaDig(String cadena){
+        ArrayList<Integer> aux = new ArrayList<>();
+        char cad;
+        int i;
+        for( i=0; i<cadena.length();i++){
+            cad=cadena.charAt(i);
+            aux.add(Character.getNumericValue(cad));
+        }
+        //System.out.println(aux.toString());
+        return aux;
     }
 
     //testeo
--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,8 @@ public class Principal {
     public static void main(String[] args)  {
         //System.out.println("Hello, World!");
         CambioBase nuevo = new CambioBase();
-        nuevo.sumaPonderada(5,34);
-        //
+        nuevo.sumaPonderada(5,"34");
+        //nuevo.divisionReiterada(2, 8);
+        //System.out.println(nuevo.listaDig("12af").toString());
     }
 }
