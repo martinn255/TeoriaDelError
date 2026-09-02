66fdb501e35a684bb7c545d931a2f39159a069f0
Claudio Vasquez <clauvzq150@gmail.com>
2026-09-02 14:46:07 -0300

    Back 1: Cambio en la clase ConversionNumero.

--- src/Principal.java
+++ src/Principal.java
@@ -2,12 +2,19 @@
 public class Principal {
     public static void main(String[] args)  {
         //System.out.println("Hello, World!");
-        CambioBase nuevo = new CambioBase();
+
 
         //nuevo.listaDig("45f");
 
         //nuevo.sumaPonderada(5,"34");
         //nuevo.divisionReiterada(2, 8);
         //System.out.println(nuevo.listaDig("12af").toString());
+
+        ConversionNumero numero = new ConversionNumero("1AB", 16);
+
+        numero.listaDig();
+
+
+
     }
 }
